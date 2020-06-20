89ffb93b16328f6b58d47da437a2ec37837623f8
sarahmelo <67019242+sarahmelo@users.noreply.github.com>
2020-06-19 21:56:37 -0300

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,62 @@
+// COMENTS
+// document.write("Hello")
+
+// VARIAVEIS - tipos de dados
+// let myvar = "He"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// STRING
+// "Isso é uma string"
+// 'Isso também é um string'
+// `Ìsso é uam string também`
+
+// NUMBER
+// const n1 = 1
+// const n1 = 12
+// document.write(n1 + n2)
+
+// BOOLEAN - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// OBJETOS - possuem propriedades e funcionalidade
+// const pessoa = {
+//  altura: "1,80m",
+//  idade: 24,
+//  solteiro: true,
+//  corre(){       
+//          document.write("executar uma grande logica de correr")
+//      }
+// }
+
+// pessoa.corre()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaDeBolinhas[3].name)
+
+// FUNÇÕES - Funcionalidades - Atalhos - Reuso de códigos
+
+// REGISTRAR FUNÇÃO
+// function sayMyName(name) {
+
+// }
+
+// EXECUTAR 
+// sayMyName("Douglas")
+// saiMyName("Ezequiel")
+// sayMyName("Claudio")
+
+// CONDICIONAIS
+
+// const notaFinal = 7
+
+//if( notaFinal < 5 ) {
+//      doment.write("Reprovado")
+// } else {
+//      document.write("Aprovado")
+// }
